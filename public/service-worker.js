6a46cde8a1b900b4ff58f65fca7f63137cd8b4da
Ann-Marie <anniedillier@gmail.com>
2020-12-06 21:53:46 -0800

    icon path

--- public/service-worker.js
+++ public/service-worker.js
@@ -11,10 +11,10 @@ const FILES_TO_CACHE = [
     "/icons/icon-96x96.png",
     "/icons/icon-128x128.png",
     "/icons/icon-144x144.png",
-    "/assets/images/icons/icon-152x152.png",
-    "/assets/images/icons/icon-192x192.png",
-    "/assets/images/icons/icon-384x384.png",
-    "/assets/images/icons/icon-512x512.png",
+    "/icons/icon-152x152.png",
+    "/icons/icon-192x192.png",
+    "/icons/icon-384x384.png",
+    "/icons/icon-512x512.png",
 ];
 // Install the service worker
 self.addEventListener("install", function (evt) {
