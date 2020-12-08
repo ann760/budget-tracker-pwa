1c033858840ff9448a5aa53ff2e2a5d87464d02b
Ann-Marie <anniedillier@gmail.com>
2020-12-07 23:02:05 -0800

    update serveiceworker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,6 @@
-const CACHE_NAME = "budget-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v2";
+const APP_PREFIX = "Budget-";
+const VERSION = "version_01";
+const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
   "/",
@@ -20,68 +21,48 @@ const FILES_TO_CACHE = [
 // Install the service worker
 self.addEventListener("install", function (e) {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log("Your files were pre-cached successfully!");
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("installing cache : " + CACHE_NAME);
       return cache.addAll(FILES_TO_CACHE);
     })
   );
-
-  self.skipWaiting();
 });
 
 // Activate the service worker and remove old data from the cache
 self.addEventListener("activate", function (e) {
   e.waitUntil(
-    cache.keys().then((keyList) => {
+    caches.keys().then(function (keyList) {
+      let cacheKeeplist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX);
+      });
+      cacheKeeplist.push(CACHE_NAME);
+
       return Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-            console.log("Removing old cache data", key);
-            return caches.delete(key);
+        keyList.map(function (key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log("deleting cache : " + keyList[i]);
+            return caches.delete(keyList[i]);
           }
         })
       );
     })
   );
-  self.clients.claim();
 });
 
 // Intercept fetch requests
 self.addEventListener("fetch", function (e) {
-  if (e.request.url.includes("/api/")) {
-    e.respondWith(
-      caches
-        .open(DATA_CACHE_NAME)
-        .then((cache) => {
-          return fetch(e.request)
-            .then((response) => {
-              // If the response was good, clone it and store it in the cache.
-              if (response.status === 200) {
-                cache.put(e.request.url, response.clone());
-              }
-
-              return response;
-            })
-            .catch((err) => {
-              // Network request failed, try to get it from the cache.
-              return cache.match(e.request);
-            });
-        })
-        .catch((err) => console.log(err))
-    );
-
-    return;
-  }
+  console.log("fetch request : " + e.request.url);
   e.respondWith(
-    fetch(evt.request).catch(function () {
-      return caches.match(e.request).then(function (response) {
-        if (response) {
-          return response;
-        } else if (e.request.headers.get("accept").includes("text/html")) {
-          // return the cached home page for all requests for html pages
-          return caches.match("/");
-        }
-      });
+    caches.match(e.request).then(function (request) {
+      if (request) {
+        // if cache is available, respond with cache
+        console.log("responding with cache : " + e.request.url);
+        return request;
+      } else {
+        // if there are no cache, try fetching request
+        console.log("file is not cached, fetching : " + e.request.url);
+        return fetch(e.request);
+      }
     })
   );
 });
