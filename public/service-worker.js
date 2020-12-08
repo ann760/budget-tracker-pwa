49685e45caaf00e91001afb8f29ecf96078f4f69
Ann-Marie <anniedillier@gmail.com>
2020-12-07 23:49:01 -0800

    fetch fix

--- public/service-worker.js
+++ public/service-worker.js
@@ -51,16 +51,16 @@ self.addEventListener("activate", function (e) {
 
 // Intercept fetch requests
 self.addEventListener("fetch", function (e) {
-  console.log("fetch request : " + e.request.url);
+ // console.log("fetch request : " + e.request.url);
   e.respondWith(
     caches.match(e.request).then(function (request) {
       if (request) {
         // if cache is available, respond with cache
-        console.log("responding with cache : " + e.request.url);
+      //  console.log("responding with cache : " + e.request.url);
         return request;
       } else {
         // if there are no cache, try fetching request
-        console.log("file is not cached, fetching : " + e.request.url);
+       // console.log("file is not cached, fetching : " + e.request.url);
         return fetch(e.request);
       }
     })
